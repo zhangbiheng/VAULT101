ec9b826a3950ba17449c484a3437a2dfea627ec4
zhangbiheng <31977640+zhangbiheng@users.noreply.github.com>
2017-12-17 23:35:44 +0800

    homework

---
+++ exer6-4.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i,a[20]={1,1,0};
+	for(i=2;i<=19;i++)
+		a[i]=a[i-1]+a[i-2];
+	cout<<"Fibonacci¨ºy¨¢D¦Ì??¡ã20??¨º?¡êo"<<endl;
+	for(i=0;i<=19;i++)
+		cout<<a[i]<<" ";
+	    cout<<endl;
+}
